ae835f980da4e293cf84ffa6c80d40238243943b
owiecc <szymon.beczkowski@me.com>
2017-03-18 21:06:51 +0100

    Converted window function into approximation function
    Returns window convolved with data

--- fn.m
+++ fn.m
@@ -1,17 +1,19 @@
-function f = fn(t,m,n)
-%FN Calculates the approximation function to be convolved with the data
+function f = fn(t,n,y)
+%FN Calculates the approximated data points
 %   t: position of sample within the window
-%   m: window size [-m:m], of 2m+1 elements
 %   n: polynomial degree
+%   y: data
+
+ny = length(y);
+m = (ny-1)/2;
 
 gff = @(a,b) prod(a-(0:b-1)); % a(a-1)(a-2)..(a-b+1) % prod(a:(a-b+1)) does not work % #nicehack
+yi = @(i,m) y(i+m+1);
 
-f = [];
+f = 0;
 for i = -m:m
-  fi = 0;
   for k = 0:n
-    fi = fi + (2*k+1)*gff(2*m,k)/gff(2*m+k+1,k+1)*grampoly(k,m,i)*grampoly(k,m,t);
+    f = f + (2*k+1)*gff(2*m,k)/gff(2*m+k+1,k+1)*grampoly(k,m,i)*grampoly(k,m,t)*yi(i,m);
   end
-  f = [f fi];
 end
 end
